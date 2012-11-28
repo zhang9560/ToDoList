b3c612503b6acedbe8a26ba1878611bb16f96471
zhang9560 <zhang9560@gmail.com>
2012-11-28 17:35:32 +0800

    Add adapter for ListFragment

--- src/app/todolist/ui/TaskListFragment.java
+++ src/app/todolist/ui/TaskListFragment.java
@@ -1,7 +1,15 @@
 package app.todolist.ui;
 
 import android.app.ListFragment;
+import android.os.Bundle;
+import app.todolist.data.TaskListAdapter;
 
 public class TaskListFragment extends ListFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        TaskListAdapter adapter = new TaskListAdapter(getActivity());
+        setListAdapter(adapter);
+    }
 }
