ec524849957853c072fff4bcd3482e913a5b5881
Yanghai Zhang <zhang9560@outlook.com>
2012-12-06 17:34:37 +0800

    Display priority picture

--- src/app/todolist/data/TaskTreeAdapter.java
+++ src/app/todolist/data/TaskTreeAdapter.java
@@ -9,6 +9,9 @@ import android.widget.CursorAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 import app.todolist.R;
+import app.todolist.utils.JOleDateTime;
+
+import java.util.Calendar;
 
 public class TaskTreeAdapter extends CursorAdapter {
 
@@ -44,13 +47,47 @@ public class TaskTreeAdapter extends CursorAdapter {
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
         ViewHolder holder = (ViewHolder)view.getTag();
+
+        holder.priority.setImageResource(priority2Res(cursor.getInt(cursor.getColumnIndex(TaskProvider.KEY_PRIORITY))));
         holder.title.setText(cursor.getString(cursor.getColumnIndex(TaskProvider.KEY_TITLE)));
-        holder.dueDate.setText(String.valueOf(cursor.getDouble(cursor.getColumnIndex(TaskProvider.KEY_DUE_DATE))));
         holder.tags.setText(cursor.getString(cursor.getColumnIndex(TaskProvider.KEY_TAGS)));
 
         int subtaskCount = cursor.getInt(cursor.getColumnIndex(TaskProvider.KEY_SUBTASK_COUNT));
         if (subtaskCount > 0) {
             holder.subtaskCount.setText(String.format("(%d)", subtaskCount));
+        }
+
+        double dateTime = cursor.getDouble(cursor.getColumnIndex(TaskProvider.KEY_DUE_DATE));
+        if(dateTime > 0) {
+            JOleDateTime dueDate = new JOleDateTime(dateTime);
+            holder.dueDate.setText(String.format("%d/%d/%d", dueDate.get(Calendar.MONTH), dueDate.get(Calendar.DAY_OF_MONTH), dueDate.get(Calendar.YEAR)));
+        }
+    }
+
+    private int priority2Res(int priority) {
+        switch (priority) {
+            case -2:
+                return R.drawable.priority_none;
+            case 0:
+                return R.drawable.priority_0;
+            case 1:
+                return R.drawable.priority_1;
+            case 2:
+                 return R.drawable.priority_2;
+            case 5:
+                return R.drawable.priority_5;
+            case 6:
+                return R.drawable.priority_6;
+            case 7:
+                return R.drawable.priority_7;
+            case 8:
+                return R.drawable.priority_8;
+            case 9:
+                return R.drawable.priority_9;
+            case 10:
+                return R.drawable.priority_10;
+            default:
+                return R.drawable.priority_none;
         }
     }
 
