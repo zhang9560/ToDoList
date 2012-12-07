3cb37383a83c8b90e233ba9073a82e44e96a1311
Yanghai Zhang <zhang9560@outlook.com>
2012-12-07 17:49:16 +0800

    Finish displaying overdue tasks

--- src/app/todolist/ui/OverdueFragment.java
+++ src/app/todolist/ui/OverdueFragment.java
@@ -1,16 +1,31 @@
 package app.todolist.ui;
 
 import android.app.ListFragment;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ListView;
 import app.todolist.R;
+import app.todolist.data.TaskProvider;
+import app.todolist.data.TaskTreeAdapter;
+import app.todolist.utils.JOleDateTime;
 
-public class OverdueFragment extends ListFragment {
+public class OverdueFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor> {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getLoaderManager().initLoader(0, null, this);
     }
 
     @Override
@@ -19,4 +34,36 @@ public class OverdueFragment extends ListFragment {
         // Don't display add task and search menu in action bar.
         menu.findItem(R.id.main_activity_menu_add_task).setVisible(false);
     }
+
+    @Override
+    public void onListItemClick(ListView listView, View item, int position, long id) {
+
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        JOleDateTime dateTime = new JOleDateTime();
+        long today = (long)dateTime.getDateTime();
+
+        String where = String.format("%s < %d", TaskProvider.KEY_DUE_DATE, today);
+        String order = String .format("%s desc, %s desc", TaskProvider.KEY_DUE_DATE, TaskProvider.KEY_PRIORITY);
+        return new CursorLoader(getActivity(), TaskProvider.TASK_URI, null, where, null, order);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        if (mAdapter == null) {
+            mAdapter = new TaskTreeAdapter(getActivity(), cursor, 0, false);
+            setListAdapter(mAdapter);
+        } else {
+            mAdapter.swapCursor(cursor);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+        mAdapter.swapCursor(null);
+    }
+
+    private TaskTreeAdapter mAdapter;
 }
