18d51cf5a5e04a129341d4fe026e95522742be8d
Yanghai Zhang <zhang9560@outlook.com>
2012-12-08 11:04:59 +0800

    Update parent's subtask count after adding a task

--- src/app/todolist/ui/NewTaskActivity.java
+++ src/app/todolist/ui/NewTaskActivity.java
@@ -3,7 +3,12 @@ package app.todolist.ui;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -13,6 +18,50 @@ import app.todolist.data.TaskProvider;
 
 public class NewTaskActivity extends Activity {
     public static final String TAG = "NewTaskActivity";
+
+    private class InsertTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            mDialog = new ProgressDialog(NewTaskActivity.this, android.R.style.Theme_Holo_Panel);
+            mDialog.setCancelable(false);
+            mDialog.setMessage(getString(R.string.please_wait));
+            mDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            ContentResolver resolver = getContentResolver();
+
+            // Insert new task to database.
+            ContentValues values = mTaskInfoFragment.getContentValues();
+            values.put(TaskProvider.KEY_PARENT_ID, mParentId);
+            resolver.insert(TaskProvider.TASK_URI, values);
+
+            // Update subtask count of new task's parent.
+            // Top level tasks have no parents, so needn't update when adding a top level task.
+            if (mParentId > 0) {
+                String[] projection = {"count(*)"};
+                String where = String.format("%s=%d", TaskProvider.KEY_PARENT_ID, mParentId);
+                Cursor cursor = resolver.query(TaskProvider.TASK_URI, projection, where, null, null);
+                if (cursor != null && cursor.moveToFirst()) {
+                    values.clear();
+                    values.put(TaskProvider.KEY_SUBTASK_COUNT, cursor.getLong(0));
+                    resolver.update(Uri.withAppendedPath(TaskProvider.TASK_URI, String.valueOf(mParentId)), values, null, null);
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            mDialog.dismiss();
+            setResult(RESULT_OK);
+            finish();
+        }
+
+        private ProgressDialog mDialog;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,11 +89,7 @@ public class NewTaskActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.new_task_activity_menu_done:
-                ContentValues values = mTaskInfoFragment.getContentValues();
-                values.put(TaskProvider.KEY_PARENT_ID, mParentId);
-                getContentResolver().insert(TaskProvider.TASK_URI, values);
-                setResult(RESULT_OK);
-                finish();
+                new InsertTask().execute();
                 return true;
         }
         return super.onOptionsItemSelected(item);
